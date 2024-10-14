fd61217838c12156b8520f182bede3bd726481e2
cheloesperguel <chelo.esperguel@gmail.com>
2024-10-14 17:18:56 -0300

    add rest_framework and cors auth packages

--- quoterapp/urls.py
+++ quoterapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('cotizar/', views.ejecutar_cotizador, name='ejecutar_cotizador'),
-    path('cotizacion_formulario/', views.RealizarCotizacion, name='Realizar_Cotizacion'),
+    path('', views.RealizarCotizacion, name='Realizar_Cotizacion'),
 ]
 
 
